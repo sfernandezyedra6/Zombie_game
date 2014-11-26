2d899777c0fa2a0778f8aeea3c2b9f4b940b2c40
sfernandezyedra6 <sfernandezyedra6@gmail.com>
2014-11-26 18:56:09 +0100

    Tarea_05-Zombie animado

---
+++ Assets/scripts/JumpScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpScript : MonoBehaviour {
+	
+	public float jumpSpeed = 5f;
+	public bool standing;
+	public float SuperJump = 2;
+	public GameObject groundCheck;
+	
+	// Update is called once per frame
+	void Update () {
+		var absVelY = Mathf.Abs(rigidbody2D.velocity.y);
+		if(absVelY <= .05f){
+			standing = true;
+		}else{
+			standing = false;
+		}
+		
+		if( (Input.GetKeyDown("up") || Input.GetKeyDown("space")) && standing){
+			rigidbody2D.AddForce(new Vector2(0, jumpSpeed), ForceMode2D.Impulse);
+		}
+	}
+	
+	//Si salimos del trigger cortamos la carga
+	void OnTriggerExit2D(Collider2D target){
+		if (target.transform.tag == "SuperJump") {
+			jumpSpeed = jumpSpeed / SuperJump;
+		}
+	}
+	
+	void OnTriggerEnter2D(Collider2D target){
+		if (target.transform.tag == "SuperJump") {
+			jumpSpeed = jumpSpeed * SuperJump;
+		}
+	}
+}
--- Assets/scripts/balaScript.cs
+++ Assets/scripts/balaScript.cs
@@ -2,10 +2,33 @@
 using System.Collections;
 
 public class balaScript : MonoBehaviour {
-
-	public Vector2D velocity = new Vector2(5,0);
-	// Use this for initialization
+	
+	
+	public Vector2 velocity = new Vector2 (5, 0);
+	public GameObject particulas;
+	
+	
 	void Start () {
-		Rigidbody2D.velocity = AudioVelocityUpdateMode * Transform.localScale.x;
-
+		//rigidbody2D.velocity = velocity * transform.localScale.x;
+		rigidbody2D.AddForce (velocity * transform.localScale.x, ForceMode2D.Impulse);
+		//En vez de velocity, al hacerlo de la manera AddForce y el ForceMode2D.Impulse la bala recibira un impulso
 	}
+	
+	
+	void OnCollisionEnter2D(Collision2D target){
+		//Destroy (gameObject);
+		explotaBala ();
+		
+		
+	}
+	
+	void explotaBala(){
+			var clone = Instantiate (particulas, transform.position, Quaternion.identity)
+				as GameObject;
+			Destroy (clone, 1);
+			Destroy (gameObject);
+		
+	}
+	
+	
+}
