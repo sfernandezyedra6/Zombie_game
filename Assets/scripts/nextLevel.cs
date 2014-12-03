2555f784f1b82704fdfc1e0902fcbc09e4ab6a67
sfernandezyedra6 <sfernandezyedra6@gmail.com>
2014-12-03 18:51:59 +0100

    Tarea_08-Pasar nivel

---
+++ Assets/scripts/nextLevel.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class nextLevel : MonoBehaviour {
+
+	public string nombreNivel = "escena_02";
+
+	public bool cargando = false;
+
+	public float tiempoEspera = 2f;
+
+	void OnTriggerStay2D(Collider2D target){
+		if (target.transform.tag == "Player") {
+			//Application.LoadLevel (nombreNivel);
+			if(!cargando)
+				StartCoroutine(cargaNivel());
+
+		}
+	}
+void OnTriggerExit2D(Collider2D target){
+	cargando = false;
+}
+
+
+IEnumerator cargaNivel(){
+	cargando = true;
+		Debug.Log ("Esperando");
+	yield return new WaitForSeconds (tiempoEspera);
+	if(cargando)
+		Application.LoadLevel (nombreNivel);
+    }
+}
