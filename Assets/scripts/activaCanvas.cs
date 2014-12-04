c4ab6d89fb5480097eff96a5e88335d185518b7c
sfernandezyedra6 <sfernandezyedra6@gmail.com>
2014-12-04 19:37:53 +0100

    Tarea_09-Hacer menú

---
+++ Assets/scripts/activaCanvas.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class activaCanvas : MonoBehaviour {
+	public GameObject canvas;
+	public bool estacerca = false;
+	public bool muestratexto = false;
+	// Use this for initialization
+	void Update () {
+		if (estacerca) {
+			canvas.SetActive(true);
+		}else{
+			canvas.SetActive(false);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		if(col.transform.tag == "Player")
+		estacerca = true;
+	}
+
+	void OnTriggerExit2D(Collider2D col){
+		if(col.transform.tag == "Player")
+		estacerca = false;
+	}	
+}
+
---
+++ Assets/scripts/generadorScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class generadorScript : MonoBehaviour {
+	public GameObject zombie;
+	public float spamTime = 5f;
+	public float maxZombies = 10;
+
+	private float nextZombie;
+	private float zcount = 0;
+	// Use this for initialization
+	void Start () {
+		nextZombie = spamTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( (Time.time > nextZombie) && (zcount < maxZombies)){
+			nextZombie = Time.time + spamTime;
+			var clone = Instantiate (zombie, transform.position, Quaternion.identity) as GameObject;
+
+			zcount++;
+		}
+	}
+}
